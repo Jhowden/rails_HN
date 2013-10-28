0bcf79398949f236a3cad9cc745df9cfa518e8e0
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 11:10:15 -0500

    routes added and skeleton controllers/actions established

---
+++ app/controllers/post_controller.rb
@@ -0,0 +1,21 @@
+class PostController < ApplicationController
+
+  def new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+end
