f470d0c8261b7d6f564109bd1995e3d1116cd567
dbc-apprentice <apprentice@devbootcamp.com>
2013-10-28 10:35:38 -0500

    Added dependent options to some models

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -3,7 +3,7 @@ class Comment < ActiveRecord::Base
   belongs_to :user
   belongs_to :post
 
-  has_many :commentvotes
+  has_many :commentvotes, dependent: :destroy
 
   validates_presence_of :content
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,8 @@
 class Post < ActiveRecord::Base
   attr_accessible :title, :url, :content
   belongs_to :user
-  has_many :comments
-  has_many :postvotes
+  has_many :comments, dependent: :destroy
+  has_many :postvotes, dependent: :destroy
 
   validates :title, presence: true
   validates :url, presence: true, unless: "!content.nil?"
